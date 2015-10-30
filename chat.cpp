ad7dc6c44b62cf20d4f4977a65713b9ad03ff062
Nickolay Kuropatkin <nictaporuk@yandex.ru>
2015-10-30 14:36:39 +0200

    init chat cpp socket

---
+++ chat.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+
+}
