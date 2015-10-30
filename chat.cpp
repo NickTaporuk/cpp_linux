b795f76a7137eb1673be95b1be96ce7b68c542b0
NickTaporuk <nictaporuk@yandex.ru>
2015-10-30 23:21:40 +0200

    continue chat

--- chat.cpp
+++ chat.cpp
@@ -7,7 +7,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+using namespace std;
+
 int main()
 {
+    int client, server;
+    int portNum = 1500;
+    bool isExists = false;
+    int bufsize = 1024;
+    char buffer[bufsize];
 
+    struct sockaddr_in_server_add;
+    socklen_t size;
+
+    //init socket
+    client = socket(AF_INET,SOCK_STREAM,0);
+
+    if(client < 0){
+        cout<<<<endl;
+    }
+    cout<<client<<endl;
 }
