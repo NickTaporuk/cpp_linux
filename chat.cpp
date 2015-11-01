38e54c0359667a11c14b508f3a62db605e74504d
NickTaporuk <nictaporuk@yandex.ru>
2015-11-01 21:42:34 +0200

    add jobs in socket

--- chat.cpp
+++ chat.cpp
@@ -6,25 +6,52 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-using namespace std;
+#include <cstdio>
 
 int main()
 {
-    int client, server;
-    int portNum = 1500;
-    bool isExists = false;
-    int bufsize = 1024;
-    char buffer[bufsize];
+    int sock, listener;
+    struct sockaddr_in addr;
+    char buf[1024];
+    int bytes_read;
+    int port = 3426;
 
-    struct sockaddr_in_server_add;
-    socklen_t size;
-
-    //init socket
-    client = socket(AF_INET,SOCK_STREAM,0);
-
-    if(client < 0){
-        cout<<<<endl;
+    listener = socket(AF_INET, SOCK_STREAM, 0);
+    if(listener < 0)
+    {
+        perror("socket");
+        exit(1);
     }
-    cout<<client<<endl;
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    if(bind(listener, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        perror("bind");
+        exit(2);
+    }
+
+    listen(listener, 1);
+
+    while(1)
+    {
+        sock = accept(listener, NULL, NULL);
+        if(sock < 0)
+        {
+            perror("accept");
+            exit(3);
+        }
+
+        while(1)
+        {
+            bytes_read = recv(sock, buf, 1024, 0);
+            if(bytes_read <= 0) break;
+            send(sock, buf, bytes_read, 0);
+        }
+
+        close(sock);
+    }
+
+    return 0;
 }
